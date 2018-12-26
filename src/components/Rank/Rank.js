85ea96eea30062db2072ebac6867b6657295c23e
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-12-26 19:09:07 +0530

    Converted Register to Smart Component and linked to Server

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
   return (
     <div>
       <div className='white f3'>
-        {`Dylan, your current entry count is...`}
+        {`${name}, your current entry count is...`}
       </div>
       <div className='white f1'>
-        {`#3`}
+        {entries}
       </div>
     </div>
   );
